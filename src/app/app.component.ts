0abe3d526897441c52920d9d7fffe0bc3d888f35
Fred <frednevesal@gmail.com>
2024-02-01 14:30:59 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'xtronh';
+ public cover = '/assets/cover.png'
+ public coverMobile = '/assets/phone.png'
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  
+
+}
